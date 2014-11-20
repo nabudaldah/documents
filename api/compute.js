897321da91757dc0e7f1c6d23f5947244432c409
nabudaldah <nabi.abudaldah@gmail.com>
2014-11-20 17:50:35 +0100

    Simplified R interface

--- api/compute.js
+++ api/compute.js
@@ -3,14 +3,17 @@ module.exports = function(app, config, db){
   /* R engine */
   console.log('Loading R module...');
   var R = require('../lib/R.js');
-  R.start(config.R.exe, config.R.dir, function(){
-    R.run(config.R.init);
+  R.start(config.R.exe, function(){
+  	if(config.R.init) R.run(config.R.init);
+    R.run('source("../lib/functions.R");cat("ready");', function(job){
+	    console.log('R engine: ' + job.log)
+    });
   });
   
   app.get('/v1/:collection/:id/compute/:script', function (req, res) {
 
     if(!req.params.collection || !req.params.id || !req.params.script) {
-      res.status(400).send(error('Missing collection, id and script parameters.')); return;
+      res.status(400).send('Missing collection, id and script parameters.'); return;
     }
 
     var fields = { _id: 1 };
@@ -19,9 +22,9 @@ module.exports = function(app, config, db){
     var collection = db.collection(req.params.collection);
     collection.findOne({ _id: req.params.id }, fields, function (err, data) {
 
-      if(err)                      { res.status(500).send(error('Database error.'));               return; }
-      if(!data)                    { res.status(500).send(error('Object not found in database.')); return; }
-      if(!data[req.params.script]) { res.status(500).send(error('Script not found.'));             return; }
+      if(err)                      { res.status(500).send('Database error.');               return; }
+      if(!data)                    { res.status(500).send('Object not found in database.'); return; }
+      if(!data[req.params.script]) { res.status(500).send('Script not found.');             return; }
 
       var init = 'context <- list(collection="' + req.params.collection + '", id="' + req.params.id + '", pid="' + process.pid + '");\n';
       var script = data[req.params.script];
