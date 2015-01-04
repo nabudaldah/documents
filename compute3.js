4c93c11f6bf251495f6dc38166b4a35c329b7b0e
Nabi Abudaldah <nabi.abudaldah@gen.nl>
2015-01-04 15:45:02 +0100

    Update computer.njs and compute3.js

--- compute3.js
+++ compute3.js
@@ -2,9 +2,9 @@ var async = require('async');
 var mongo = require('mongojs');
 var db    = mongo.connect('documents');
 
-function compute3(){
+function compute3(callback){
 
-  var verbose = false;
+  var verbose = true;
 
   var query  = { tags: { $in: ["computation", "data"] } };
   var fields = { _id: 1, tags: 1, update: 1, dependencies: 1, computation: 1 };
@@ -14,6 +14,7 @@ function compute3(){
     db.collection('computations').find(query, fields, function(err, data) {
       documentsList = data;
       if(verbose) console.log('compute3: getDocuments: ' + (new Date() - t0))
+      if(verbose) console.log('compute3: getDocuments: data.length: ' + data.length)
       callback();
     });
   };
@@ -32,38 +33,51 @@ function compute3(){
 
   function listComputations(callback){
     var t0 = new Date();
+    var Nx = 0;
     for(c in computationDocuments){
       var parent = computationDocuments[c];
       if(!parent.dependencies) continue;
       var id = parent.dependencies.split('/')[1];
       var child = documents[id];
-      if(new Date(parent.update) < new Date(child.update))
-        computations.push(parent);
+      if(new Date(parent.update) < new Date(child.update)){
+        computations[parent._id] = parent._id;
+        Nx++;
+      }      
     }
     if(verbose) console.log('compute3: listComputations: ' + (new Date() - t0))
+    if(verbose) console.log('compute3: listComputations: Nx: ' + Nx)
     callback();
   };
 
   function queueComputations(callback){
-    computations.map(function(computation){
+    var t0 = new Date();
+    for(id in computations){
+      var computation = documents[id];
       var init   = 'context <- list(collection="' + 'computations' + '", id="' + computation._id + '");\n';
       var script = computation.computation;
-      run(init + script);
-    })
+      //run(init + script);      
+      N++;
+    }
+    if(verbose) console.log('compute3: queueComputations: ' + (new Date() - t0));
+    callback();
   };
 
   function finish(err){
-    if(verbose) console.log('compute3: done. need ' + computations.length + ' computations');
-    //db.close();
+    if(verbose) console.log('compute3: done. need ' + N + ' computations');
+    if(callback && typeof(callback) == 'function') callback();
   };
 
+  var N = 0;
   var documentsList = [];
   var documents     = {};
   var computationDocuments = {};
   var dataDocuments = {}; 
-  var computations  = [];
-  async.series([getDocuments, sortDocuments, listComputations], finish);
+  var computations  = {};
+  async.series([getDocuments, sortDocuments, listComputations, queueComputations], finish);
 
 }
 
-compute3();
+
+compute3(function(){
+  db.close();
+});
--- computer.njs
+++ computer.njs
@@ -171,7 +171,7 @@ function compute2(){
 
 function compute3(callback){
 
-  var verbose = false;
+  var verbose = true;
 
   var query  = { tags: { $in: ["computation", "data"] } };
   var fields = { _id: 1, tags: 1, update: 1, dependencies: 1, computation: 1 };
@@ -181,6 +181,7 @@ function compute3(callback){
     db.collection('computations').find(query, fields, function(err, data) {
       documentsList = data;
       if(verbose) console.log('compute3: getDocuments: ' + (new Date() - t0))
+      if(verbose) console.log('compute3: getDocuments: data.length: ' + data.length)
       callback();
     });
   };
@@ -199,39 +200,46 @@ function compute3(callback){
 
   function listComputations(callback){
     var t0 = new Date();
+    var Nx = 0;
     for(c in computationDocuments){
       var parent = computationDocuments[c];
       if(!parent.dependencies) continue;
       var id = parent.dependencies.split('/')[1];
       var child = documents[id];
-      if(new Date(parent.update) < new Date(child.update))
-        computations.push(parent);
+      if(new Date(parent.update) < new Date(child.update)){
+        computations[parent._id] = parent._id;
+        Nx++;
+      }      
     }
     if(verbose) console.log('compute3: listComputations: ' + (new Date() - t0))
+    if(verbose) console.log('compute3: listComputations: Nx: ' + Nx)
     callback();
   };
 
   function queueComputations(callback){
     var t0 = new Date();
-    computations.map(function(computation){
+    for(id in computations){
+      var computation = documents[id];
       var init   = 'context <- list(collection="' + 'computations' + '", id="' + computation._id + '");\n';
       var script = computation.computation;
-      run(init + script);
-    })
-    if(verbose) console.log('compute3: queueComputations: ' + (new Date() - t0))
+      //run(init + script);      
+      N++;
+    }
+    if(verbose) console.log('compute3: queueComputations: ' + (new Date() - t0));
+    callback();
   };
 
   function finish(err){
-    if(verbose) console.log('compute3: done. need ' + computations.length + ' computations');
-    //db.close();
+    if(verbose) console.log('compute3: done. need ' + N + ' computations');
     if(callback && typeof(callback) == 'function') callback();
   };
 
+  var N = 0;
   var documentsList = [];
   var documents     = {};
   var computationDocuments = {};
   var dataDocuments = {}; 
-  var computations  = [];
+  var computations  = {};
   async.series([getDocuments, sortDocuments, listComputations, queueComputations], finish);
 
 }
