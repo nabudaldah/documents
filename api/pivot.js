8a6f47daadefa87f672f8f6f92d7675401b67e22
Nabi Abudaldah <nabi.abudaldah@gmail.com>
2015-07-17 20:50:27 +0200

    Pivot added summation as default

--- api/pivot.js
+++ api/pivot.js
@@ -20,7 +20,7 @@ module.exports = function(context){
     var match
     if(pivot.tags && pivot.tags.length) match = { "_tags": { "$all": pivot.tags } }
 
-    var group = { _id: null }
+    var group = { _id: null, 'count': { '$sum': 1 } }
     if(pivot.by) {      
       for(index in pivot.by) {
         var field = pivot.by[index]
@@ -29,15 +29,14 @@ module.exports = function(context){
       }
     }
 
-    if(!pivot.measures || !pivot.measures.length) { 
-      stderr('Missing request body measures.'); 
-      res.status(400).send('Missing request body measures.'); 
-      return }
-    for(index in pivot.measures){
-      var opp   = pivot.measures[index][0]
-      var field = pivot.measures[index][1]
-      var agg = {}; agg['$' + opp] = '$' + field
-      group[opp + '_' + field] = agg
+    if(pivot.measures && pivot.measures.length) {
+      group['count'] = { '$sum': 1 }
+      for(index in pivot.measures){
+        var opp   = pivot.measures[index][0]
+        var field = pivot.measures[index][1]
+        var agg = {}; agg['$' + opp] = '$' + field
+        group[opp + '_' + field] = agg
+      }
     }
 
     var pipeline = []
