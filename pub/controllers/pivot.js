f2d56f059c2178c995f53b674c9809194204e163
Nabi Abudaldah <nabi.abudaldah@gmail.com>
2015-07-18 02:26:09 +0200

    Pivot UI update

--- pub/controllers/pivot.js
+++ pub/controllers/pivot.js
@@ -4,27 +4,42 @@ ctrl.controller('pivot', function ($scope, $http, $window, $location, messages) 
   $scope.api  = '/api/' + $scope.object;
   $scope.route = '/' + $scope.object;
 
-  $scope.dimensions = [];
-  $scope.selection = []
-  $scope.aggregation = "COUNT";
-
   $scope.gridOptions = { data: null }
 
   $http.get($scope.api + '/?query=template').success(function(list){
-    for(var l in list){
-      $http.get($scope.api + '/' + list[l]._id).success(function(template){
-        if(template._template){
-          for(key in template._template){
-            var field = template._template[key].name;
-            if($scope.dimensions.indexOf(field) == -1){
-              $scope.dimensions.push(field);
-              $scope.selection.push(field);
-            }
-          };
-        }
-      });
-    };
+    $scope.templates = list.map(function(template){ return(template._id) });
+    $scope.updateFields()
   });
+
+  $scope.toggleSearch = function(){
+    $scope.showSearch = (!$scope.showSearch)
+    console.log($scope.showSearch)
+  }
+
+  $scope.showMaxFields = 5
+
+  $scope.updateFields = function(){
+    $scope.dimensions = [];
+    $scope.selection = []
+    for(var id in $scope.templates){
+      if(($scope.template && $scope.template + '' == $scope.templates[id] + '') || !$scope.template)
+        $http.get($scope.api + '/' + $scope.templates[id]).success(function(template){
+          if(template._template){
+            for(key in template._template){
+              var field = template._template[key].name;
+              if($scope.dimensions.indexOf(field) == -1){
+                $scope.dimensions.push(field);
+                $scope.selection.push(field);
+              }
+            };
+          }
+      });
+    };    
+  }
+
+  $scope.$watch('tags', function(newValue, oldValue) {
+    $scope.load()
+  }, true);
 
   $scope.filter = function(){
     $scope.selection = []
@@ -67,25 +82,30 @@ ctrl.controller('pivot', function ($scope, $http, $window, $location, messages) 
 
     var query = { measures: $scope.measures, by: $scope.group }
 
+    if($scope.tags && $scope.tags.length) query.tags = $scope.tags;
+
     $http.post(url, query)
     .success(function (data, status, headers, config){
-      var columns = [];
+      var columnsGroup   = [];
+      var columnsMeasure = [];
       var flat = [];
       console.log(status)
       console.log(data)
       if(data && data.length){
         data.map(function(item){
           var record = {}
-          for(key in item._id) { record[key] = item._id[key]; columns.push(key) }
-          for(key in item) if(key != '_id') { record[key] = item[key]; columns.push(key) }
+          for(key in item._id) { record[key] = item._id[key]; columnsGroup.push(key) }
+          for(key in item) if(key != '_id') { record[key] = item[key]; columnsMeasure.push(key) }
           flat.push(record)
         })        
       }
       $scope.data = flat;
       $scope.dataFmt = JSON.stringify($scope.data, null, 2)
-      columns = _.unique(columns)
-      var columnDefs = columns.map(function(column){ return({ field: column })})
-      console.log(columnDefs)
+      columnsGroupUnique   = _.unique(columnsGroup)
+      columnsMeasureUnique = _.unique(columnsMeasure)
+      var columnDefsGroup    = columnsGroupUnique.map(  function(column){ return({ field: column })})
+      var columnDefsMeasures = columnsMeasureUnique.map(function(column){ return({ field: column, cellFilter: 'number', cellClass: 'grid-align' })})
+      var columnDefs = columnDefsGroup.concat(columnDefsMeasures)
       // $scope.gridOptions = { data: [{ a: 'a1', b: 'b1' }, { a: 'a2', b: 'b2' }], columnDefs: [{field: 'a'}, {field:'b'}] }
       $scope.gridOptions = { data: JSON.parse(JSON.stringify(flat)), columnDefs: columnDefs }
 
