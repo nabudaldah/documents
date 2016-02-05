3569b6f6bc24b0ca91884f9bd528052f6afa147e
Nabi Abudaldah <nabi.abudaldah@gmail.com>
2016-02-05 22:15:32 +0100

    added public R client

---
+++ pub/client/DataBox.R
@@ -0,0 +1,45 @@
+library(httr)
+
+DataBox <- setRefClass("DataBox",
+  fields = list(
+    user = "character",
+    pass = "character",
+    url  = "character"
+  ),
+  methods = list(
+    initialize = function(..., user = NULL, pass = NULL, url = "https://databoxapp.com/api"){
+      callSuper(..., user = user, pass = pass, url = url)
+    },
+    open = function(user, pass) {
+      user <<- user
+      pass <<- pass
+    },
+    list = function(query = '', skip = 0, limit = 24) {
+      auth <- authenticate(user, pass, type = "basic")
+      conf <- config(ssl_verifypeer = F)
+      urlx <- paste0(url, '/', user, '?query=', query, '&skip=', skip, '&limit=', limit)
+      result <- GET(urlx, auth, conf)
+      warn_for_status(result)
+      result <- content(result)
+      return(result)
+    },
+    get = function(id) {
+      auth <- authenticate(user, pass, type = "basic")
+      conf <- config(ssl_verifypeer = F)
+      urlx <- paste0(url, '/', user, '/', id)
+      result <- GET(urlx, auth, conf)
+      warn_for_status(result)
+      result <- content(result)
+      return(result)
+    },
+    set = function(id, obj) {
+      auth <- authenticate(user, pass, type = "basic")
+      conf <- config(ssl_verifypeer = F)
+      urlx <- paste0(url, '/', user, '/', id)
+      result <- POST(urlx, body = obj, encode = "json", auth, conf)
+      warn_for_status(result)
+      status <- http_status(result)
+      return(status$message)
+    }
+  )
+)
